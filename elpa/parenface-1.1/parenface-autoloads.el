da61f62aadc114de559687a38ccbf86d36fddb52
jamieorc <jamie@dang.com>
2011-11-16 10:23:36 -0500

    Initial emacs.d commit.

---
+++ elpa/parenface-1.1/parenface-autoloads.el
@@ -0,0 +1,18 @@
+;;; parenface-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("parenface-pkg.el" "parenface.el") (20148
+;;;;;;  38164 742917))
+
+;;;***
+
+(provide 'parenface-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; parenface-autoloads.el ends here
---
+++ init.el
@@ -0,0 +1,28 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+
+(custom-set-variables
+ '(global-linum-mode t))
+
+(setq linum-format "%5d ")
+(setq mac-option-modifier 'meta
+      scroll-conservatively 101)
+
+(global-set-key (kbd "A-o") 'ido-find-file)
+(global-set-key (kbd "s-R") 'eval-last-sexp)
+(global-set-key [s-right] 'move-end-of-line)
+(global-set-key [s-left] 'move-beginning-of-line)
+(global-set-key [s-up] 'beginning-of-buffer)
+(global-set-key [s-down] 'end-of-buffer)
+(global-set-key (kbd "s-0") 'delete-window)
+(global-set-key (kbd "s-1") 'delete-other-windows)
+(global-set-key (kbd "s-2") 'split-window-below)
+(global-set-key (kbd "s-3") 'split-window-right)
+(global-set-key (kbd "s->") 'other-window)
+(global-set-key (kbd "s-}") 'next-buffer)
+(global-set-key (kbd "s-{") 'prv-buffer)
+
+
