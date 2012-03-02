082490ebde1ef67dece167b23c8fbe6130ccb7cc
jamieorc <jamie@dang.com>
2012-03-02 10:18:14 -0500

    init.el: updated faces.

--- init.el
+++ init.el
@@ -5,6 +5,10 @@
 
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(global-linum-mode t))
 
 (setq linum-format "%5d ")
@@ -26,3 +30,9 @@
 (global-set-key (kbd "s-{") 'prv-buffer)
 
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Monaco")))))
