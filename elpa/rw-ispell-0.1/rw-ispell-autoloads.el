fe7340f20efcb5d59d8dfa90f19a821ed7571afe
jamieorc <jamie@dang.com>
2012-03-14 10:26:12 -0400

    magit: updated to 1.1.1 and removed simple keys. rw-ispell: installed.

---
+++ elpa/rw-ispell-0.1/rw-ispell-autoloads.el
@@ -0,0 +1,18 @@
+;;; rw-ispell-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("rw-ispell-pkg.el" "rw-ispell.el") (20318
+;;;;;;  42573 448680))
+
+;;;***
+
+(provide 'rw-ispell-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; rw-ispell-autoloads.el ends here
